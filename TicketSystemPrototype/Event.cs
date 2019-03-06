7d1d8db1244ae9243be7d4cb118bb9108d84c12a
Mikael Ås Johannessen <mikael.a.johannessen@hiof.no>
2019-03-06 21:53:13 +0100

    Added Ticket class with a constructor with inheritance from Event

--- TicketSystemPrototype/Event.cs
+++ TicketSystemPrototype/Event.cs
@@ -1,5 +1,30 @@
 ﻿using System;
 namespace TicketSystemPrototype
 {
-    public class Event     {         string Name { get; set; }         DateTime Date { get; set; }         string Adress { get; set; }         DateTime AgeLimit { get; set; }         float TicketPrice { get; set; }         string EventInfo { get; set; }           public Event(string name, DateTime date, string adress, DateTime agelimit, float ticketprice, string eventinfo)         {             this.Name = name;             this.Date = date;             this.Adress = adress;             this.AgeLimit = agelimit;             this.TicketPrice = ticketprice;             this.EventInfo = eventinfo;         }          public override string ToString()         {             return Name + " " + Date + " " + Adress + " " + AgeLimit + " " + TicketPrice + " " + EventInfo;         }     } 
+    public class Event
+    {
+        string Name { get; set; }
+        DateTime Date { get; set; }
+        string Adress { get; set; }
+        DateTime AgeLimit { get; set; }
+        float TicketPrice { get; set; }
+        string EventInfo { get; set; }
+
+
+        public Event(string name, DateTime date, string adress, DateTime agelimit, float ticketprice, string eventinfo)
+        {
+            this.Name = name;
+            this.Date = date;
+            this.Adress = adress;
+            this.AgeLimit = agelimit;
+            this.TicketPrice = ticketprice;
+            this.EventInfo = eventinfo;
+        }
+
+        public override string ToString()
+        {
+            return Name + " " + Date + " " + Adress + " " + AgeLimit + " " + TicketPrice + " " + EventInfo;
+        }
+    }
+
 }
---
+++ TicketSystemPrototype/Ticket.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TicketSystemPrototype
+{
+    public class Ticket : Event
+    {
+
+        int TicketID { get; set; }
+
+        public Ticket (int ticketid, string name, DateTime date, string adress, DateTime agelimit, float ticketprice, string eventinfo) : base (name,date,adress,agelimit,ticketprice,eventinfo){
+            this.TicketID = TicketID;
+        }
+}   
+
+
+
+
+
+}
