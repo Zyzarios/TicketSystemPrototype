c71d41731b924e47e172a6cd6099f2b9daf36ce4
Suzdar Ibrahim <suzdaribrahim@Suzdars-MacBook-Pro.local>
2019-03-06 10:37:48 +0100

    Test

--- TicketSystemPrototype/Program.cs
+++ TicketSystemPrototype/Program.cs
@@ -9,6 +9,7 @@ namespace TicketSystemPrototype
             int x = 42;
            
             //Test André jejjejneioee
+            // Test Suzz
             var TrackDay = new Event("Trackday 2019", new DateTime(2019, 5, 2), "Motorsportveien 180, 1890 Rakkestad", new DateTime(2003, 01, 01), 460.00f, " Driving on the...");
 
             Console.WriteLine(TrackDay.ToString());             Console.ReadLine();         }
