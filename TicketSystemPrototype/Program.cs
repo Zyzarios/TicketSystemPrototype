457b7c0fbe45f99cbaf49de1e4f89ac56596937c
Suzdar Ibrahim <shirnetflix@gmail.com>
2019-03-06 10:43:18 +0100

    added deez nuts

--- TicketSystemPrototype/Program.cs
+++ TicketSystemPrototype/Program.cs
@@ -11,6 +11,7 @@ namespace TicketSystemPrototype
             //Test André jejjejneioee
             // Test Suzz
             // Saba
+            // DEEZ NUTS
             var TrackDay = new Event("Trackday 2019", new DateTime(2019, 5, 2), "Motorsportveien 180, 1890 Rakkestad", new DateTime(2003, 01, 01), 460.00f, " Driving on the...");
 
             Console.WriteLine(TrackDay.ToString());             Console.ReadLine();         }
