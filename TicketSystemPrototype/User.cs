a62bcf1337eadb972edc6ae08efc888096bbf226
sinjensen <skjensen@hiof.no>
2019-03-07 12:09:29 +0100

    Added constructor in User.cs

--- TicketSystemPrototype/User.cs
+++ TicketSystemPrototype/User.cs
@@ -1,8 +1,18 @@
 ﻿using System;
 namespace TicketSystemPrototype
 {
-    class User     {         string Username { get; set; }         string Password { get; set; }         string FirstName { get; set; }         string LastName { get; set; }         string Adress { get; set; }         string PhoneNumber { get; set; }         string Email { get; set; }         Event[] PurchasedEvents { get; set; }
+    public class User     {         string Username { get; set; }         string Password { get; set; }         string FirstName { get; set; }         string LastName { get; set; }         string Address { get; set; }         string PhoneNumber { get; set; }         string Email { get; set; }         Event[] PurchasedEvents { get; set; }
 
+        public User(string username, string password, string firstname, string lastname, string address, string phoneNumber, string email)
+        {
+            this.Username = username;
+            this.Password = password;
+            this.FirstName = firstname;
+            this.LastName = lastname;
+            this.Address = address;
+            this.PhoneNumber = phoneNumber;
+            this.Email = email;
+        }
 
     }
 }
